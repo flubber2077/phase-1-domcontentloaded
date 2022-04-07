a4c3cd8d620f7d6122d8c9ea45016b96d4993300
Dylan Jordan <kyle.dylan.jordan@gmail.com>
2022-04-07 14:36:48 -0400

    commit tree 1ca89c763655d285716ae6d21b011b77376448bb

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("The DOM has loaded"); //change the html id text to This is really cool
+    document.getElementById('text').textContent = "This is really cool!";
+});
+
+console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+);
